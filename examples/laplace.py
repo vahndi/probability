9ae396867eb16ef21c04233de2d00e962f970d16
vahndi.minah <vahndi.minah@frogdesign.com>
2020-01-05 17:12:38 -0500

    add Laplace distribution

---
+++ examples/laplace.py
@@ -0,0 +1,39 @@
+import matplotlib.pyplot as plt
+from numpy.ma import arange
+
+from probability.distributions.continuous.laplace import Laplace
+from probability.plots import new_axes
+
+x = arange(-10, 10.1, 0.1)
+
+
+def plot_pdfs():
+    """
+    https://en.wikipedia.org/wiki/Laplace_distribution#/media/File:Laplace_pdf_mod.svg
+    """
+    ax = new_axes()
+    Laplace(mu=0, b=1).plot(x=x, color='red', ax=ax)
+    Laplace(mu=0, b=2).plot(x=x, color='black', ax=ax)
+    Laplace(mu=0, b=4).plot(x=x, color='blue', ax=ax)
+    Laplace(mu=-5, b=4).plot(x=x, color='green', ax=ax)
+    ax.legend(loc='upper right')
+    plt.show()
+
+
+def plot_cdfs():
+    """
+    https://en.wikipedia.org/wiki/Laplace_distribution#/media/File:Laplace_cdf_mod.svg
+    """
+    ax = new_axes()
+    Laplace(mu=0, b=1).cdf().plot(x=x, color='red', ax=ax)
+    Laplace(mu=0, b=2).cdf().plot(x=x, color='black', ax=ax)
+    Laplace(mu=0, b=4).cdf().plot(x=x, color='blue', ax=ax)
+    Laplace(mu=-5, b=4).cdf().plot(x=x, color='green', ax=ax)
+    ax.legend(loc='lower right')
+    plt.show()
+
+
+if __name__ == '__main__':
+
+    plot_pdfs()
+    plot_cdfs()
---
+++ probability/distributions/continuous/laplace.py
@@ -0,0 +1,42 @@
+from scipy.stats import laplace, rv_continuous
+
+from probability.distributions.mixins.rv_continuous_mixin import RVContinuousMixin
+
+
+class Laplace(RVContinuousMixin):
+
+    def __init__(self, mu: float, b: float):
+
+        self._mu: float = mu
+        self._b: float = b
+        self._reset_distribution()
+
+    def _reset_distribution(self):
+        self._distribution: rv_continuous = laplace(self._mu, self._b)
+
+    @property
+    def mu(self) -> float:
+
+        return self._mu
+
+    @mu.setter
+    def mu(self, value: float):
+        self._mu = value
+        self._reset_distribution()
+
+    @property
+    def b(self) -> float:
+        return self._b
+
+    @b.setter
+    def b(self, value: float):
+        self._b = value
+        self._reset_distribution()
+
+    def __str__(self):
+
+        return f'Laplace(μ={self._mu}, b={self._b})'
+
+    def __repr__(self):
+
+        return f'Laplace(mu={self._mu}, b={self._b})'
