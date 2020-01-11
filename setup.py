ce2e120e2b6285da1c025fbfbad3f65b13e7e47a
vahndi.minah <vahndi.minah@frogdesign.com>
2020-01-11 16:23:02 -0500

    update setup

---
+++ setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+setup(
+  name='probability',
+  packages=['probability'],
+  version='0.0.1',
+  license='MIT',
+  description='Probability in Python 3',
+  author='Vahndi Minah',
+  url='https://github.com/vahndi/probability',
+  download_url='https://github.com/vahndi/probability/archive/v_0.0.1.tar.gz',
+  keywords=['python', 'probability'],
+  install_requires=[
+        'matplotlib',
+        'numpy',
+        'pandas',
+        'scipy'
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
