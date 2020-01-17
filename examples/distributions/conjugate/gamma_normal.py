b72c0af72105a20874db0102cf037ae265c5dbf8
vahndi.minah <vahndi.minah@frogdesign.com>
2020-01-17 15:31:32 -0500

    add GammaNormal conjugate distribution

---
+++ examples/distributions/conjugate/gamma_normal.py
@@ -0,0 +1,39 @@
+import matplotlib.pyplot as plt
+from numpy import arange
+from scipy import randn
+from scipy.stats import norm
+
+from probability.distributions.conjugate.gamma_normal import GammaNormal
+from probability.plots import new_axes
+
+x = arange(0, 1.01, 0.001)
+mu, sigma = 2, 3
+x_i = mu + sigma * randn(1000)
+dist = GammaNormal(alpha=1, beta=1, x=x_i, mu=2)
+
+
+def plot_parameters():
+
+    ax = new_axes()
+    dist.prior().plot(x=x, color='r', ax=ax)
+    dist.posterior().plot(x=x, color='g', ax=ax)
+    ax.legend()
+    plt.show()
+
+
+def plot_predictions():
+
+    ax = new_axes()
+    predicted = dist.rvs(100000)
+    ax.hist(predicted, bins=100, density=True, label='PPD samples')
+    x_actual = arange(predicted.min(), predicted.max(), 0.01)
+    actual = norm(loc=mu, scale=sigma).pdf(x_actual)
+    ax.plot(x_actual, actual, label='True Distribution')
+    ax.legend()
+    plt.show()
+
+
+if __name__ == '__main__':
+
+    plot_parameters()
+    plot_predictions()
---
+++ probability/distributions/conjugate/gamma_normal.py
@@ -0,0 +1,95 @@
+from math import sqrt
+
+from numpy import array, ndarray, power
+from scipy.stats import t, rv_continuous
+
+from probability.custom_types import RVMixin, Array1d
+from probability.distributions import Gamma
+from probability.distributions.continuous.inv_gamma import InvGamma
+from probability.distributions.mixins.conjugate_mixin import ConjugateMixin
+from probability.distributions.mixins.rv_continuous_1d_mixin import RVContinuous1dMixin
+
+
+class GammaNormal(RVContinuous1dMixin, ConjugateMixin):
+
+    def __init__(self, alpha: float, beta: float,
+                 x: Array1d, mu: float):
+        self._alpha: float = alpha
+        self._beta: float = beta
+        self._x: ndarray = array(x)
+        self._mu = mu
+        self._reset_distribution()
+
+    def _reset_distribution(self):
+        self._distribution: rv_continuous = t(
+            2 * self.alpha_prime,
+            loc=self._mu,
+            scale=sqrt(self.beta_prime / self.alpha_prime)
+        )
+
+    @property
+    def alpha_prime(self) -> float:
+        return self._alpha + self.n / 2
+
+    @property
+    def beta_prime(self) -> float:
+        return self._beta + (self.n / 2) * power(self._x - self._mu, 2).mean()
+
+    @property
+    def alpha(self) -> float:
+        return self._alpha
+
+    @alpha.setter
+    def alpha(self, value: float):
+        self._alpha = value
+        self._reset_distribution()
+
+    @property
+    def beta(self) -> float:
+        return self._beta
+
+    @beta.setter
+    def beta(self, value: float):
+        self._beta = value
+        self._reset_distribution()
+
+    @property
+    def x(self) -> ndarray:
+        return self._x
+
+    @x.setter
+    def x(self, value: Array1d):
+        self._x = value
+        self._reset_distribution()
+
+    @property
+    def n(self) -> int:
+        return len(self._x)
+
+    @property
+    def mu(self) -> float:
+        return self._mu
+
+    @mu.setter
+    def mu(self, value: float):
+        self._mu = value
+        self._reset_distribution()
+
+    def prior(self) -> Gamma:
+        return InvGamma(
+            alpha=self._alpha, beta=self._beta
+        ).with_x_label('τ').prepend_to_label('Prior: ')
+
+    def likelihood(self, **kwargs) -> RVMixin:
+        pass
+
+    def posterior(self, **kwargs) -> Gamma:
+        return Gamma(
+            alpha=self.alpha_prime, beta=self.beta_prime
+        ).with_x_label('τ').prepend_to_label('Posterior: ')
+
+    def __str__(self):
+        return f'GammaNormal(α={self._alpha}, β={self._beta}, μ={self._mu}, n={self.n})'
+
+    def __repr__(self):
+        return f'GammaNormal(alpha={self._alpha}, beta={self._beta}, mu={self._mu}, n={self.n})'
