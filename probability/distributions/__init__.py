8e8fffd567315fd3e0d763c856f4885f20bba760
vahndi.minah <vahndi.minah@frogdesign.com>
2020-01-20 11:58:25 -0500

    v_0.0.4

--- probability/distributions/__init__.py
+++ probability/distributions/__init__.py
@@ -1,2 +1,4 @@
+from probability.distributions.conjugate import *
 from probability.distributions.continuous import *
 from probability.distributions.discrete import *
+from probability.distributions.multivariate import *
---
+++ probability/distributions/conjugate/__init__.py
@@ -0,0 +1,5 @@
+from probability.distributions.conjugate.beta_binomial import BetaBinomial
+from probability.distributions.conjugate.gamma_exponential import GammaExponential
+from probability.distributions.conjugate.gamma_normal import GammaNormal
+from probability.distributions.conjugate.inv_gamma_normal import InvGammaNormal
+from probability.distributions.conjugate.normal_normal import NormalNormal
