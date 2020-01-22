351ebbff23e5249463971d643d56a275c63bd4f3
vahndi.minah <vahndi.minah@frogdesign.com>
2020-01-22 13:35:55 -0500

    working implementation of joint distribution

--- examples/joint_distribution.py
+++ examples/joint_distribution.py
@@ -2,27 +2,40 @@ from pandas import DataFrame
 
 from probability.joint_distribution import JointDistribution
 
+
+def underline(msg):
+    print()
+    print(msg)
+    print('=' * len(msg))
+
+
 data = DataFrame({
     'box': ['red'] * 8 + ['blue'] * 4,
     'fruit': ['apple'] * 2 + ['orange'] * 7 + ['apple'] * 3
 })
+underline('data')
+print(data)
+
 
 jd = JointDistribution(data)
 
-print(jd.p('fruit', box='red'))
-print(jd.p('fruit', 'box', box='red'))
-print()
+underline("P(fruit|box=red)")
+print(jd.p('fruit', _box='red'))
 
-print(jd.p('fruit', box='blue'))
-print(jd.p('fruit', 'box', box='blue'))
-print()
-print()
+underline("P(fruit,box|box=red)")
+print(jd.p('fruit', 'box', _box='red'))
 
+underline('P(fruit|box=blue)')
+print(jd.p('fruit', _box='blue'))
+
+underline('P(fruit,box|box=blue)')
+print(jd.p('fruit', 'box', _box='blue'))
+
+underline('P(fruit=orange|box=blue)')
+print(jd.p(fruit='orange', _box='blue'))
+
+underline('P(fruit)')
 print(jd.p('fruit'))
-print()
 
+underline('P(box)')
 print(jd.p('box'))
-print()
-print()
-
-print(jd.p('fruit', 'box', box='red'))
--- probability/joint_distribution.py
+++ probability/joint_distribution.py
@@ -1,59 +1,144 @@
 from pandas import concat, DataFrame, Series
-from typing import List, Tuple, Any
+import re
+from typing import Any, List, Optional
 
 
-class Condition(object):
+class VariableFilter(object):
 
-    def __init__(self, var_name: str, comparator: str, value: Any):
+    re_var_comparator = re.compile(
+        r'(\w+)__((?:eq)|(?:ne)|(?:lt)|(?:gt)|(?:le)|(?:ge)|(?:in)|(?:not_in))'
+    )
+    var_name: str
+    comparator: Optional[str]
+    value: Any
+    conditional: bool
+    comparator_symbols = {
+        'eq': lambda arg, val: f'{arg}={val}',
+        'ne': lambda arg, val: f'{arg}≠{val}',
+        'lt': lambda arg, val: f'{arg}<{val}',
+        'gt': lambda arg, val: f'{arg}>{val}',
+        'le': lambda arg, val: f'{arg}≤{val}',
+        'ge': lambda arg, val: f'{arg}≥{val}',
+        'in': lambda arg, vals: '{}∈{}'.format(
+            arg, '{' + ",".join([str(val) for val in vals]) + '}'
+        ),
+        'not_in': lambda arg, vals: '{}∉{}'.format(
+            arg, '{' + ",".join([str(val) for val in vals]) + '}'
+        )
+    }
 
-        self.var_name: str = var_name
-        self.comparator: str = comparator
-        self.value: Any = value
+    def __init__(self, arg_name: str, var_names: List[str], arg_value: Any = None):
+
+        if arg_value is None:
+            # no conditioning value given
+            if arg_name not in var_names:
+                raise ValueError(f'{arg_name} is not a variable in the dataset.')
+            self.var_name = arg_name
+            self.comparator = None
+            self.value = None
+            self.conditional = False
+        else:
+            # conditioning value given
+            match = self.re_var_comparator.match(arg_name)
+            if not match:
+                if arg_name in var_names:
+                    var_name, comparator = arg_name, 'eq'
+                elif arg_name in ['_' + n for n in var_names]:
+                    var_name, comparator = arg_name[1:], 'eq'
+                else:
+                    raise ValueError(f'Could not match {arg_name}.')
+            else:
+                var_name, comparator = match.groups()
+            # set var_name
+            if arg_name in var_names:
+                self.var_name = arg_name
+                self.conditional = False
+            elif arg_name in ['_' + n for n in var_names]:
+                self.var_name = arg_name[1:]
+                self.conditional = True
+            self.var_name: str = var_name
+            # set comparator
+            self.comparator = comparator
+            self.value = arg_value
+
+    def get_name(self) -> str:
+
+        return self.comparator_symbols[self.comparator](self.var_name, self.value)
+
+    def get_filter(self, data: DataFrame) -> Series:
+
+        if self.comparator is None:
+            return Series(data=[True]*len(data), index=data.index, name=self.var_name)
+
+        name = self.get_name()
+        compare: Series = data[self.var_name]
+        if self.comparator == 'eq':
+            return Series(data=compare == self.value, name=name)
+        elif self.comparator == 'ne':
+            return Series(data=compare != self.value, name=name)
+        elif self.comparator == 'lt':
+            return Series(data=compare < self.value, name=name)
+        elif self.comparator == 'gt':
+            return Series(data=compare > self.value, name=name)
+        elif self.comparator == 'le':
+            return Series(data=compare <= self.value, name=name)
+        elif self.comparator == 'ge':
+            return Series(data=compare >= self.value, name=name)
+        elif self.comparator == 'in':
+            return Series(data=compare.isin(self.value), name=name)
+        elif self.comparator == 'not_in':
+            return Series(data=~compare.isin(self.value), name=name)
 
 
 class JointDistribution(object):
 
     def __init__(self, data: DataFrame):
 
+        # TODO: check column names are unique
+        # TODO: check each column name can be a python variable
+        # TODO: check each column name is not equal to '_' plus another column name
         self._data: DataFrame = data
-        self._variables: List[str] = list(self._data.columns)
-        self._conditions: List[Tuple[str, str, Any]] = []  # var_name, comparator, value
-        self._marginals: List[str] = []
-
+        self._variables = list(self._data.columns)
         for variable in self._variables:
-            try:
-                setattr(self, variable, self._data[variable])
-            except:
-                print('Warning - could not set variable {}'.format(variable))
+            setattr(self, variable, self._data[variable])
 
     def p(self, *args, **kwargs) -> Series:
 
         data = self._data
-        # get margins
-        margins = [var_name for var_name in args]
-        # get conditions
-        conditions = [Condition(var_name, '=', value) for var_name, value in kwargs.items()]
-        # apply conditions
-        if conditions:
-            condition_filters = []
-            for condition in conditions:
-                condition_filter = self._data[condition.var_name] == condition.value
-                condition_filters.append(condition_filter)
-            conditions_filter: Series = concat(condition_filters, axis=1).all(axis=1)
-            data = data.loc[conditions_filter]
-        # remove marginalized and conditional variables
-        data = data.loc[:, margins]
-        # sum over remaining variables
-        data = data.groupby(margins).size()
-        # normalize
-        data = data / data.sum()
-        # return result
-        data.name = 'p({}{}{})'.format(
-            ','.join(margins),
-            '|' if conditions else '',
-            ','.join([
-                '{}{}{}'.format(condition.var_name, condition.comparator, condition.value)
-                for condition in conditions
-            ])
-        )
-        return data
+        # get filters
+        filters: List[VariableFilter] = [
+            VariableFilter(arg_name=arg, var_names=self._variables)
+            for arg in args
+        ] + [
+            VariableFilter(arg_name=kw_arg, var_names=self._variables, arg_value=kw_value)
+            for kw_arg, kw_value in kwargs.items()
+        ]
+        # conditionals e.g. c1 in p(j1, j2=a|c1=b)
+        conditionals: List[VariableFilter] = [f for f in filters if f.conditional]
+        if conditionals:
+            condition_filters: DataFrame = concat(
+                [f.get_filter(self._data) for f in conditionals], axis=1
+            )
+            conditions_filter: Series = condition_filters.all(axis=1)
+            conditioned_data = data.loc[conditions_filter].copy()
+        else:
+            conditioned_data = data.copy()
+        # marginals e.g. j2 in p(j1, j2=a|c1=b)
+        marginals: List[VariableFilter] = [f for f in filters if not f.conditional and f.value is not None]
+        marginal_names = [m.var_name for m in marginals]
+        if marginals:
+            marginal_filters: DataFrame = concat(
+                [f.get_filter(conditioned_data) for f in marginals], axis=1
+            )
+            marginals_filter: Series = marginal_filters.all(axis=1)
+            # replace data in each marginal with its marginal comparator and value (to make result explicit)
+            for marginal in marginals:
+                conditioned_data.loc[:, marginal.var_name] = marginal.get_name()
+            marginal_data = conditioned_data.loc[marginals_filter]
+        else:
+            marginal_data = conditioned_data
+        # fulls e.g. j1 in p(j1, j2=a|c1=b)
+        fulls: List[VariableFilter] = [f for f in filters if not f.conditional and f.value is None]
+        full_names = [f.var_name for f in fulls]
+        final_data = marginal_data.groupby(full_names + marginal_names).size() / len(conditioned_data)
+        return final_data
