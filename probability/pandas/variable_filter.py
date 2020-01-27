8297457edf4191493557d1f0543d832aa10a31ae
vahndi.minah <vahndi.minah@frogdesign.com>
2020-01-27 18:28:28 -0500

    remove DataSet and retain VariableFilter class for later use

--- probability/pandas/variable_filter.py
+++ probability/pandas/variable_filter.py
@@ -1,8 +1,6 @@
-from pandas import concat, DataFrame, Series
+from pandas import DataFrame, Series
 import re
 from typing import Any, List, Optional
-
-from probability.pandas.discrete_distribution import DiscreteDistribution
 
 
 class VariableFilter(object):
@@ -90,73 +88,3 @@ class VariableFilter(object):
             return Series(data=compare.isin(self.value), name=name)
         elif self.comparator == 'not_in':
             return Series(data=~compare.isin(self.value), name=name)
-
-
-class DataSet(object):
-
-    def __init__(self, data: DataFrame):
-
-        var_names = list(data.columns)
-        # data validation
-        if len(var_names) != len(set(var_names)):
-            raise ValueError('Data column names must be unique.')
-        non_identifiers = [var_name for var_name in var_names if not var_name.isidentifier()]
-        if non_identifiers:
-            raise ValueError('The following names cannot be used as variables: {}.'.format(non_identifiers))
-        potential_errors = [var_name for var_name in var_names if var_name.startswith('_')]
-        if potential_errors:
-            raise ValueError('The following names could cause errors on conditioning: {}.'.format(potential_errors))
-        # set member variables
-        self._data: DataFrame = data
-        self._var_names: List[str] = var_names
-        for variable in self._var_names:
-            setattr(self, variable, self._data[variable])
-
-    def joint(self) -> DiscreteDistribution:
-
-        data = (
-            self._data.groupby(self._var_names).size() /
-            len(self._data)
-        ).rename('p')
-        return DiscreteDistribution(data=data, conditionals=None)
-
-    def p(self, *args, **kwargs) -> Series:
-
-        data = self._data
-        # get filters
-        filters: List[VariableFilter] = [
-            VariableFilter(arg_name=arg, var_names=self._var_names)
-            for arg in args
-        ] + [
-            VariableFilter(arg_name=kw_arg, var_names=self._var_names, arg_value=kw_value)
-            for kw_arg, kw_value in kwargs.items()
-        ]
-        # conditionals e.g. c1 in p(j1,j2=a|c1=b)
-        conditionals: List[VariableFilter] = [f for f in filters if f.conditional]
-        if conditionals:
-            condition_filters: DataFrame = concat(
-                [f.get_filter(self._data) for f in conditionals], axis=1
-            )
-            conditions_filter: Series = condition_filters.all(axis=1)
-            conditioned_data = data.loc[conditions_filter].copy()
-        else:
-            conditioned_data = data.copy()
-        # marginals e.g. j2 in p(j1,j2=a|c1=b)
-        marginals: List[VariableFilter] = [f for f in filters if not f.conditional and f.value is not None]
-        marginal_names = [m.var_name for m in marginals]
-        if marginals:
-            marginal_filters: DataFrame = concat(
-                [f.get_filter(conditioned_data) for f in marginals], axis=1
-            )
-            marginals_filter: Series = marginal_filters.all(axis=1)
-            # replace data in each marginal with its marginal comparator and value (to make result explicit)
-            for marginal in marginals:
-                conditioned_data.loc[:, marginal.var_name] = marginal.get_name()
-            marginal_data = conditioned_data.loc[marginals_filter]
-        else:
-            marginal_data = conditioned_data
-        # fulls e.g. j1 in p(j1,j2=a|c1=b)
-        fulls: List[VariableFilter] = [f for f in filters if not f.conditional and f.value is None]
-        full_names = [f.var_name for f in fulls]
-        final_data = marginal_data.groupby(full_names + marginal_names).size() / len(conditioned_data)
-        return final_data
