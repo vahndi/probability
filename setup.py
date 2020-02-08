fa86beafec37e63ef3fe707d2aaf31cf6f27d0e4
vahndi.minah <vahndi.minah@frogdesign.com>
2020-02-08 14:57:22 -0500

    v 0.0.8

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages
 setup(
   name='probability',
   packages=find_packages(),
-  version='0.0.7',
+  version='0.0.8',
   license='MIT',
   description='Probability in Python 3',
   author='Vahndi Minah',
