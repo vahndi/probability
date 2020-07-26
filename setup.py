3cb154e8debae22111f98be2f54cfad9b498e629
vahnd <vahndi.minah@gmail.com>
2020-07-26 16:11:06 -0400

    v0,.0.136

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages
 setup(
   name='probability',
   packages=find_packages(),
-  version='0.0.135',
+  version='0.0.136',
   license='MIT',
   description='Probability in Python 3',
   author='Vahndi Minah',
