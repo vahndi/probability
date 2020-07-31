6064137d6abf1e1cbb76ff58363ca210cd1ccb14
vahndi.minah <vahndi.minah@frogdesign.com>
2020-07-31 09:11:18 -0400

    move tests for Beta infer to BetaBinomial infer

--- tests/test_conjugate/test_beta_binomial.py
+++ tests/test_conjugate/test_beta_binomial.py
@@ -2,10 +2,10 @@ from unittest.case import TestCase
 
 from pandas import Series, DataFrame
 
-from probability.distributions import Beta
+from probability.distributions import Beta, BetaBinomial
 
 
-class TestBeta(TestCase):
+class TestBetaBinomial(TestCase):
 
     def setUp(self) -> None:
 
@@ -17,14 +17,14 @@ class TestBeta(TestCase):
             'd': [1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0]
         })
 
-    def test_from_series(self):
+    def test_infer_posterior(self):
 
         expected = Beta(alpha=4, beta=6)
-        actual = Beta.infer(self.series)
+        actual = BetaBinomial.infer_posterior(self.series)
         self.assertEqual(expected.alpha, actual.alpha)
         self.assertEqual(expected.beta, actual.beta)
 
-    def test_from_data_frame(self):
+    def test_infer_posteriors(self):
 
         expected = DataFrame([
             {'a': 1, 'b': 1, 'p': 'c', 'Beta': Beta(0, 3)},
@@ -36,7 +36,7 @@ class TestBeta(TestCase):
             {'a': 1, 'b': 2, 'p': 'd', 'Beta': Beta(1, 2)},
             {'a': 2, 'b': 2, 'p': 'd', 'Beta': Beta(0, 3)}
         ])
-        actual = Beta.infers(
+        actual = BetaBinomial.infer_posteriors(
             data=self.data_frame,
             prob_vars=['c', 'd'],
             cond_vars=['a', 'b']
