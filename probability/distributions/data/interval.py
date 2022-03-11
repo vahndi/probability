1a3db0de85615b6cb37d025ebd686869967ba648
vahndi <vahndi.minah@gmail.com>
2022-03-11 12:07:10 -0500

    add some random generators to Interval distribution

--- probability/distributions/data/interval.py
+++ probability/distributions/data/interval.py
@@ -1,6 +1,7 @@
-from typing import Optional
+from typing import Optional, Union
 
-from numpy.random import seed
+from numpy import arange
+from numpy.random import seed, choice, uniform
 from pandas import Series
 
 from probability.distributions.mixins.data_mixins import \
@@ -24,12 +25,64 @@ class Interval(
 
     def __init__(self, data: Series):
         """
-        Create a new Ordinal distribution.
+        Create a new Interval distribution.
 
         :param data: pandas Series of interval data.
         """
         data = data.dropna()
         self._data: Series = data
+
+    @staticmethod
+    def random(
+            name: str,
+            min_value: Union[int, float],
+            max_value: Union[int, float],
+            interval: Union[int, float],
+            size: int
+    ):
+        """
+        Generate an interval distribution where the probability of observing each
+        value comes from a uniform distribution.
+
+        :param name: Name for the distribution and underlying data Series.
+        :param min_value: Lowest value to generate.
+        :param max_value: Highest value to generate.
+        :param interval: Size of the interval between each value.
+        :param size: Total number of samples to generate.
+        """
+        a = arange(start=min_value, stop=max_value + interval, step=interval)
+        p = Series(uniform(size=len(a)))
+        p /= p.sum()
+        data = Series(
+            data=choice(a=a, p=p, size=size),
+            name=name
+        )
+        return Interval(data=data)
+
+    @staticmethod
+    def random_uniform(
+            name: str,
+            min_value: Union[int, float],
+            max_value: Union[int,float],
+            interval: Union[int, float],
+            size: int
+    ) -> 'Interval':
+        """
+        Generate an interval distribution with uniformly distributed
+        values.
+
+        :param name: Name for the distribution and underlying data Series.
+        :param min_value: Lowest value to generate.
+        :param max_value: Highest value to generate.
+        :param interval: Size of the interval between each value.
+        :param size: Total number of samples to generate.
+        """
+        a = arange(start=min_value, stop=max_value + interval, step=interval)
+        data = Series(
+            data=choice(a=a, size=size),
+            name=name
+        )
+        return Interval(data=data)
 
     def rvs(self, num_samples: int,
             random_state: Optional[int] = None) -> Series:
