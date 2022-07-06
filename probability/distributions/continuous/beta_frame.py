d4bbb7950d55bd90cce22ff8a4773243dace327c
vahndi <vahndi.minah@gmail.com>
2022-07-06 10:24:32 -0400

    extend BetaFrame

--- probability/distributions/continuous/beta_frame.py
+++ probability/distributions/continuous/beta_frame.py
@@ -1,6 +1,6 @@
 from typing import List, Union
 
-from pandas import DataFrame, concat
+from pandas import DataFrame, concat, Series
 
 from probability.distributions.continuous.beta_series import BetaSeries
 from probability.distributions.mixins.continuous_frame_mixin import \
@@ -12,13 +12,29 @@ class BetaFrame(
     object
 ):
 
-    def __init__(self, data: DataFrame):
+    def __init__(
+            self,
+            data: Union[DataFrame, Series]
+    ):
         """
         Create a new BetaFrame.
 
-        :param data: DataFrame of Beta distributions.
+        :param data: DataFrame of Beta distributions, or Series of BetaSeries.
         """
-        self._data: DataFrame = data
+        if isinstance(data, DataFrame):
+            self._data: DataFrame = data
+        elif isinstance(data, Series):
+            self._data = DataFrame({
+                key: data[key].data
+                for key in data.keys()
+            }).T
+        else:
+            raise TypeError('must pass DataFrame or Series of BetaSeries')
+
+    @property
+    def T(self) -> 'BetaFrame':
+
+        return BetaFrame(self._data.T)
 
     @staticmethod
     def from_beta_series(data: List[BetaSeries]):
